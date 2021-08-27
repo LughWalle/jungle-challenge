8fadd27ca48280ff2a3538d330355f044cff0653
LughWalle <civalfran1@gmail.com>
2021-08-27 17:14:20 -0300

    configured App.js and created components with Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header';
+
+function Hero() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <div>
+          <h1>Easily create or join a local nanny share with Hapu</h1>
+          <h6>Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.</h6>
+          <div>
+            <h6>See hapu in action (27 seconds)</h6>
+          </div>
+        </div>
+        <div>imagem aqui</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
