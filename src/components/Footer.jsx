4ead83d81345ff29d179fb42a72d4faec1f02775
LughWalle <civalfran1@gmail.com>
2021-08-29 11:15:09 -0300

    created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <h2>Become a nanny share host</h2>
+      <p>Takes less than 5 minutes to get started</p>
+      <div className="div-button">
+        <div>icon button</div>
+        <div>
+          <h5>Create Your Nanny Share</h5>
+          <h6>Takes less than 5 minutes</h6>
+        </div>
+      </div>
+      <div>
+        <div>icon-logo</div>
+        <div>
+          <a href="/">Share your Nanny</a>
+          <a href="/">Our Story</a>
+          <a href="/">Blog</a>
+          <a href="/">Terms &amp; Privacy</a>
+        </div>
+        <div>
+          <div>fb</div>
+          <div>tw</div>
+          <div>itg</div>
+        </div>
+      </div>
+      <h6>Copyright © 2017 Hapu PTY Limited All rights reserved</h6>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -20,6 +20,26 @@ function Infos() {
           <button typr="button">send</button>
         </div>
       </div>
+      <div>
+        <img src="/" alt="img" />
+        <div>
+          <h2>Shared payments made simple</h2>
+          <p>Sometimes it’s hard enough just sharing a restaurant bill. Try sharing that bill week in, week out and you might encounter more than a few snares. But not with Hapu. Simply set your rates and our automated payment system takes care of the rest. You need never talk money or who owes what.</p>
+          <a href="/">Read how Bridget’s share (without Hapu) ended over $15</a>
+        </div>
+      </div>
+      <div>
+        <h2>A framework built for the long term</h2>
+        <p>Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.</p>
+        <a href="/">Read how Hapu’s tribal background defines our app</a>
+        <div>imagem aqui</div>
+      </div>
+      <div>
+        <div>svg's aqui</div>
+        <h2>Coming soon: Nanny Share Daily Diary!</h2>
+        <p>With the Hapu daily diary your nanny will be able to update you and your sharers with photos and commentary of the day. You and sharers will receive notifications and you’ll be able to login to view the daily adventures fo your little ones. We can’t wait!</p>
+        
+      </div>
     </div>
   )
 }
