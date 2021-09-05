4ba91a6fa2b4709d1a92a0f4de29d9464169b65e
LughWalle <civalfran1@gmail.com>
2021-09-05 13:33:13 -0300

    style of footer in progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './sass/Footer.scss';
 import hapu_logo from '../assets/vectors/hapu_logo.svg';
 import fb_logo from '../assets/vectors/facebook_logo.svg';
 import itg_logo from '../assets/vectors/instagram_logo.svg';
@@ -7,31 +8,32 @@ import calendar from '../assets/vectors/calendar_icon.svg';
 
 function Footer() {
   return (
-    <div>
-      <h2>Become a nanny share host</h2>
+    <div className="footer">
+      <h1>Become a nanny share host</h1>
       <p>Takes less than 5 minutes to get started</p>
-      <div className="div-button">
+      <button type="button" className="btn-create">
         <img src={calendar} alt="icon calendar" />
         <div>
-          <h5>Create Your Nanny Share</h5>
-          <h6>Takes less than 5 minutes</h6>
+          <p>Create Your Nanny Share</p>
+          <p>Takes less than 5 minutes</p>
         </div>
-      </div>
-      <div>
-        <img src={hapu_logo} alt="" />
-        <div>
+      </button>
+      <a className="nannys-shared" href="/">Or browse local nanny-shares</a>
+      <div className="nav-footer">
+        <img className="hapu" src={hapu_logo} alt="name hapu" />
+        <div className="nav-menu">
           <a href="/">Share your Nanny</a>
           <a href="/">Our Story</a>
           <a href="/">Blog</a>
           <a href="/">Terms &amp; Privacy</a>
         </div>
-        <div>
+        <div className="social">
           <img src={fb_logo} alt="facebook logo" />
           <img src={tw_logo} alt="twitter logo" />
           <img src={itg_logo} alt="instagram logo" />
         </div>
       </div>
-      <h6>Copyright © 2017 Hapu PTY Limited All rights reserved</h6>
+      <p className="copyrigth">Copyright © 2017 Hapu PTY Limited All rights reserved</p>
     </div>
   )
 }
